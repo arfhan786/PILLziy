8f2e1eea22c3367cfe399be19169cc4e459300a0
Arfhan <arfhanmahmad@gmail.com>
2026-01-24 23:50:34 -0600

    Initial Commit

---
+++ Pillzy/PillzyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PillzyApp.swift
+//  Pillzy
+//
+//  Created by Arfhan Ahmad on 1/24/26.
+//
+
+import SwiftUI
+
+@main
+struct PillzyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
